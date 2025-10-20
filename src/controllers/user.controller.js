6c376eb880a7256c27cfb52ce3a0033ae0ccdf92
Muhammad Rizwan <mrmrkhan10000@gmail.com>
2025-10-20 22:32:18 +0500

    all system is configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 export const registerUser = (req, res) => {
 //   const { name, email, password } = req.body;
   res.status(201).json({
-    message: "User registered successfully,Welcome ali khan",
+    message:"User registered successfully",
     // user: { name, email }
   });
 };
