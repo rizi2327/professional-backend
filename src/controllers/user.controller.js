def3103953381506fbaaf52c9497d8cfd08b5b5d
rizi2327 <mrmrkhan10000@gmail.com>
2025-11-22 20:29:47 +0500

    feat: implement getWatchHistory function to fetch user watch history with video details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -580,3 +580,54 @@ return res
 .status(200)
 .json(new ApiResponse(200,channel[0],"user profile fetched successfully"))
 })
+
+export  const getWatchHistory=asyncHandler(async(req,res)=>
+{
+  //get user watch history from req.user
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id:new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistoryVideos",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as :"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+            }
+          },{
+            $addFields:{
+              owner:{
+                // $arrayElemAt:["$owner",0]
+                $first:"$owner"
+              }
+            }
+          }
+        ]
+      }
+    },
+  ])
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,user[0].watchHistory,"user watch history fetched successfully"
+  ))
+})
